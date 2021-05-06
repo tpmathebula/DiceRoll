0ab8ce50f3f748ce58a3cfe1831d49e3aeb1ec56
Themba Mathebula <themba.mathebula@entelect.co.za>
2021-05-06 18:32:52 +0200

    Refactored the dice app by using lateinit and vector drawable compatibility library

--- app/src/main/java/com/example/diceroller1/MainActivity.kt
+++ app/src/main/java/com/example/diceroller1/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,14 +19,13 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
 
+        diceImage = findViewById(R.id.dice_image)
 
     }
 
     private fun rollDice() {
 
         val randomInt =  Random.nextInt(6) + 1
-
-        val diceImage : ImageView = findViewById(R.id.dice_image)
 
         val drawableResource = when(randomInt){
             1->R.drawable.dice_1
